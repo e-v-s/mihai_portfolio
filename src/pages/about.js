a5d61f7dd5b54099a8fc21e111eca9c6b32f650a
Évora evs Leite <evoraleite@gmail.com>
2020-12-29 14:48:15 -0500

    Finish about

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,11 @@ function About() {
 	useEffect(() => {
 		window.scrollTo(0, 0)
 	  }, []);
-	  
+
 	return(
 		<div className={css(style.page)}>
 			<p className={css(style.text)}>
-				Graduated as an Electrical Engineer at the Technical Univeristy of Köln (Cologne) in Germany in 2005. Have been involved with the Automation and SCADA Industry before University graduation, I have been working more than 10 years as Project Engineer, System Integrator, SCADA/PLC and ADMS specialist. The last 10 years were in the Oil&Gas and Utilities Industries.
+				I graduated as an Electrical Engineer at the Technical Univeristy of Köln (Cologne) in Germany in 2005. Have been involved with the Automation and SCADA Industry before University graduation and been working more than 10 years as Project Engineer, System Integrator, SCADA/PLC and ADMS specialist. The last 10 years were in the Oil&Gas and Utilities Industries.
 			</p>
 			<p className={css(style.text)}>
 				Working in the Oil&Gas industry as a member of expert engineering teams designing/customizing and implementing complete control systems, I could gain a good grasp of many of the aspects and involved processes of the daily Oil&Gas operations and the required automation and control systems.
@@ -29,6 +29,7 @@ function About() {
 			<p className={css(style.text)}>
 				In the last years I started focusing on Turbine Controls System, have worked as Lead Programmer on several gas turbines projects with Woodward and Allen Bradley Controls.
 			</p>
+			<div className={css(style.textDiv)}></div>
 		</div>
 	)
 }
@@ -37,20 +38,24 @@ const style = StyleSheet.create({
 	page: {
 		display: 'inline',
 	},
-	title: {
-		fontFamily: `'Roboto', sans-serif`,
-		textTransform: 'uppercase',
-		border: '1px solid #000',
-		width: '150px',
-		padding: '30px 10px',
-		textAlign: 'center',
-		float: 'left',
-		margin: '20px',
-	},
 	text: {
 		fontFamily: `'Roboto', sans-serif`,
-		textAlign: 'justify',
-		lineHeight: '24px'
+		textAlign: 'left',
+		lineHeight: '30px',
+		width: '90%',
+		margin: '0 auto',
+		marginBottom: '30px',
+		color: '#031940',
+		fontSize: '18px',
+		'@media only screen and (min-width: 600px)': {
+			textIndent: '4em',
+			width: '70%',
+		}
+	},
+	textDiv: {
+		'width': '60%',
+		borderBottom: '5px solid #D7D7D9',
+		margin: '60px auto'
 	}
 });
 
